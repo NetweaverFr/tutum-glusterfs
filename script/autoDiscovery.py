84594de55e5e67bdab62e0f6b5d7b7d508405afc
Sébastien Lemarinel <slemarinel@oulala.com>
2015-09-19 22:35:01 +0200

    update autodiscover

--- script/autoDiscovery.py
+++ script/autoDiscovery.py
@@ -33,9 +33,9 @@ while True:
 
             # get peers informations
             command = ['gluster', 'peer', 'status']
-            statePeer = subprocess.Popen(command, stderr=subprocess.STDOUT)
+            statePeer = subprocess.Popen(command, stdout=subprocess.PIPE)
             # get the peer response with the peers IP
-            stdout = statePeer.stdout.read()
+            stdout = process.communicate()[0]
 
             for container in containers:
                 peer = re.match(container.__getattribute__('private_ip'), stdout)
